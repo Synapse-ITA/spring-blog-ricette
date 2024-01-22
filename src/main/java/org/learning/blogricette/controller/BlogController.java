eb153a736470614aba7c8687b3ec68eb06b858ef
Synapse-ITA <puggillovalerio@gmail.com>
2024-01-22 13:55:43 +0100

    Bonus start & finish + refactoring HTML

---
+++ src/main/java/org/learning/blogricette/controller/BlogController.java
@@ -0,0 +1,51 @@
+package org.learning.blogricette.controller;
+
+import org.learning.blogricette.model.Categoria;
+import org.learning.blogricette.model.Ricetta;
+import org.learning.blogricette.repository.CategoriaRepository;
+import org.learning.blogricette.repository.RicettaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/blog")
+public class BlogController {
+    @Autowired
+    private RicettaRepository ricettaRepository;
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public String index(Model model) {
+        List<Ricetta> ricettaList = ricettaRepository.findAll();
+        List<Categoria> categoriaList = categoriaRepository.findAll();
+
+        model.addAttribute("ricettaList", ricettaList);
+        model.addAttribute("categoriaList", categoriaList);
+
+        return "blog/list";
+    }
+
+    @GetMapping("/ricetta/{id}")
+    public String detailsRicetta(@PathVariable Integer id, Model model) {
+        Optional<Ricetta> ricetta = ricettaRepository.findById(id);
+        if (ricetta.isPresent()) {
+            model.addAttribute("ricetta", ricetta.get());
+            return "ricetta/details";
+        } else {
+            return "redirect:/blog";
+        }
+    }
+
+
+
+
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO ricette (name, ingredients, image, preparation_time, num_of_portions, description) VALUES ('Cake', '200g Farina, 5 Uova', 'UrlIMG', 20, 1, 'Torta semplice al cioccolato');
+INSERT INTO ricette (name, ingredients, image, preparation_time, num_of_portions, description) VALUES ('Cake', '200g Farina, 5 Uova', 'https://www.cucchiaio.it/content/dam/cucchiaio/it/ricette/2023/05/torta-chantilly/torta-chantilly-aperture.jpg', 20, 1, 'Torta semplice al cioccolato');
 
 INSERT INTO categorie (name) VALUES ('Dolci');
