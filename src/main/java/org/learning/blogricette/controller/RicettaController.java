68d32d2044a56863c414bee697834993cd079912
Synapse-ITA <puggillovalerio@gmail.com>
2024-01-22 10:54:20 +0100

    list.html + create.html + sql fix

--- src/main/java/org/learning/blogricette/controller/RicettaController.java
+++ src/main/java/org/learning/blogricette/controller/RicettaController.java
@@ -1,11 +1,15 @@
 package org.learning.blogricette.controller;
 
+import jakarta.validation.Valid;
 import org.learning.blogricette.model.Ricetta;
 import org.learning.blogricette.repository.RicettaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,6 +24,22 @@ public class RicettaController {
         List<Ricetta> ricettaList = ricettaRepository.findAll();
         model.addAttribute("ricettaList", ricettaList);
         return "ricetta/list";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        Ricetta ricetta = new Ricetta();
+        model.addAttribute("ricetta", ricetta);
+        return "ricetta/create";
+    }
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("ricetta") Ricetta formRicetta, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "ricetta/create";
+        } else {
+            Ricetta savedRicetta = ricettaRepository.save(formRicetta);
+            return "redirect:/ricetta";
+        }
     }
 
 
