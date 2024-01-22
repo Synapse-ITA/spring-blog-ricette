3af3d385d5f47dd8128a63194490ae24f663b245
Synapse-ITA <puggillovalerio@gmail.com>
2024-01-22 09:29:20 +0100

    Project Start

---
+++ src/test/java/org/learning/blogricette/BlogricetteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.blogricette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogricetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
